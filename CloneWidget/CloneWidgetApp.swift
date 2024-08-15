bd1f08cb632bd6c7a2955396b1a0de6c054a5288
minoh.park <parkmino86@gmail.com>
2024-08-15 19:32:12 +0900

    Initial Commit

---
+++ CloneWidget/CloneWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneWidgetApp.swift
+//  CloneWidget
+//
+//  Created by minoh.park on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CloneWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
