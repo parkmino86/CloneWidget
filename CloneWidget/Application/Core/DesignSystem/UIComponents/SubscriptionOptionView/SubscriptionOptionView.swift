927bd679b4e3feada40c9dd9049fa5f99f66bb0b
minoh.park <parkmino86@gmail.com>
2024-09-22 14:39:15 +0900

    feat: SubscriptionOptionView 및 관련 UI 컴포넌트 추가
    - 구독 옵션 선택을 위한 ScrollView 및 VStack UI 컴포넌트 구현
    - SubscriptionOptionCore를 통해 구독 옵션 상태 관리
    - SubscriptionOptionButtonCore를 사용하여 개별 구독 옵션 버튼의 선택 상태 처리

---
+++ CloneWidget/Application/Core/DesignSystem/UIComponents/SubscriptionOptionView/SubscriptionOptionView.swift
@@ -0,0 +1,80 @@
+//
+//  SubscriptionOptionView.swift
+//  CloneWidget
+//
+//  Created by minoh.park on 9/22/24.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - View
+
+struct SubscriptionOptionView: View {
+    let store: StoreOf<SubscriptionOptionCore>
+
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 12) {
+                ForEachStore(
+                    store.scope(state: \.options, action: SubscriptionOptionCore.Action.subscriptionOption(id:action:))
+                ) {
+                    SubscriptionOptionButton(store: $0)
+                        .padding(.horizontal, 16)
+                }
+            }
+            .padding(.vertical, 8)
+        }
+    }
+}
+
+// MARK: - Reducer
+
+@Reducer
+struct SubscriptionOptionCore {
+    @ObservableState
+    struct State: Equatable {
+        var options: IdentifiedArrayOf<SubscriptionOptionButtonCore.State> = []
+    }
+
+    enum Action: Equatable {
+        case subscriptionOption(id: UUID, action: SubscriptionOptionButtonCore.Action)
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .subscriptionOption(id, .didSelectSubscriptionOption):
+                // 구독 옵션 리스트에서 선택된 옵션 업데이트
+                state.options = IdentifiedArray(
+                    uniqueElements: state.options.map { option in
+                        var updatedOption = option
+                        updatedOption.isSelected = option.id == id
+                        return updatedOption
+                    }
+                )
+                return .none
+            }
+        }
+        .forEach(\.options, action: /Action.subscriptionOption(id:action:)) {
+            SubscriptionOptionButtonCore()
+        }
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    SubscriptionOptionView(
+        store: Store(
+            initialState: SubscriptionOptionCore.State(
+                options: [
+                    SubscriptionOptionButtonCore.State(name: "Option 1", price: 5500, isSelected: false),
+                    SubscriptionOptionButtonCore.State(name: "Option 2", price: 11000, isSelected: true),
+                    SubscriptionOptionButtonCore.State(name: "Option 3", price: 16500, isSelected: false)
+                ]
+            ),
+            reducer: { SubscriptionOptionCore() }
+        )
+    )
+}
